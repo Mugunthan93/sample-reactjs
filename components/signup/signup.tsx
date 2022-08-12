3c6552fb6c5ee6d5063fe8514eba9ec381be90eb
Mugunthan93 <kskarthick93@gmail.com>
2022-08-12 17:31:58 +0530

    1 file modified

--- components/signup/signup.tsx
+++ components/signup/signup.tsx
@@ -1,15 +1,41 @@
 import * as React from 'react';
+import { useState } from 'react';
 import './signup.scss';
 
 const Signup = () => {
+  let [loginForm, setLoginForm] = useState({
+    username: '',
+    password: '',
+  });
+
+  const submit = () => {
+    // prevents the submit button from refreshing the page
+    event.preventDefault();
+    console.log(loginForm);
+  };
+
+  const handleChange = (event) => {
+    setLoginForm({ ...loginForm, [event.target.name]: event.target.value });
+  };
+
   return (
-    <form className="signupform">
+    <form onSubmit={submit} className="signupform">
       <h1 className="formtitle">Signup Form</h1>
       <div className="inputs">
         <h3>UserName : </h3>
-        <input type="text" />
+        <input
+          type="text"
+          name="username"
+          onChange={handleChange}
+          value={loginForm.username}
+        />
         <h3>Password : </h3>
-        <input type="password" />
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={loginForm.password}
+        />
         <button>Submit</button>
       </div>
     </form>
@@ -17,3 +43,7 @@ const Signup = () => {
 };
 
 export default Signup;
+
+function SetState() {
+  throw new Error('Function not implemented.');
+}
