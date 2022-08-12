8acabcb878b2628e4f61b2ebe0390f9e06170e07
Mugunthan93 <kskarthick93@gmail.com>
2022-08-12 16:25:35 +0530

    signup UI created

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
+import './app.scss';
 import './style.css';
 
-export default () => {
+const App = () => {
   return (
-    <h1>Hi </h1>
+    <form className="signupform">
+      <h1 className="formtitle">Signup Form</h1>
+      <div className="inputs">
+        <h3>UserName : </h3>
+        <input type="text" />
+        <h3>Password : </h3>
+        <input type="password" />
+        <button>Submit</button>
+      </div>
+    </form>
   );
-}
+};
+
+export default App;
