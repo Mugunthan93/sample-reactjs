9c5fc2647feaa26f1e0701f4a045e68a4c14bf84
Mugunthan93 <kskarthick93@gmail.com>
2022-08-12 11:56:26 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './style.css';
+
+export default () => {
+  return (
+    <h1>Hi </h1>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-tteq9b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-tteq9b)
